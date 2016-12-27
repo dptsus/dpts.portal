a0b726e1f9ea6752167afaf57136674a99879090
Tushar <khairnar.tk@hotmail.com>
2016-12-27 11:35:27 +0530

    test commit

--- DPTS/DPTS.Web/Controllers/DoctorController.cs
+++ DPTS/DPTS.Web/Controllers/DoctorController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DPTS.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,7 +9,14 @@ namespace DPTS.Web.Controllers
 {
     public class DoctorController : Controller
     {
-        // GET: Customer
+        private ApplicationDbContext context;
+
+        public DoctorController()
+        {
+            context = new ApplicationDbContext();
+        }
+
+        // GET: Doctor
         public ActionResult Info()
         {
             return View();
@@ -16,6 +24,7 @@ namespace DPTS.Web.Controllers
 
         public ActionResult ProfileSetting()
         {
+            //var info=context.Users.fi
             return View();
         }
     }
