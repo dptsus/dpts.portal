fc2e07fd161208f130d362cbe0357db5e46fd34b
maheshshankhpal@gmail.com <mahesh.shankhpal@gmail.com>
2017-02-13 23:32:20 +0530

    ReviewComments Service & Interface added

---
+++ DPTS/DPTS.Domain/Core/ReviewComments/IReviewComments.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using DPTS.Domain.Entities;
+
+namespace DPTS.Domain.Core.ReviewComments
+{
+    public interface IReviewComments
+    {
+        IList<Entities.ReviewComments> GetAllAprovedReviewCommentsByUser(string UserId);
+    }
+}
---
+++ DPTS/DPTS.Services/ReviewComments/ReviewCommentsService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using DPTS.Domain.Core.ReviewComments;
+using DPTS.Domain.Entities;
+using DPTS.Domain.Core;
+using System.Linq;
+
+namespace DPTS.Domain.ReviewComments
+{
+    public class ReviewCommentsService : IReviewComments
+    {
+        #region Fields
+        private readonly IRepository<Domain.Entities.ReviewComments> _reviewComments;
+        #endregion
+
+        #region Constructor
+        public ReviewCommentsService(IRepository<Domain.Entities.ReviewComments> reviewComments)
+        {
+            _reviewComments = reviewComments;
+        }
+        #endregion
+
+        #region Methods
+        IList<Entities.ReviewComments> IReviewComments.GetAllAprovedReviewCommentsByUser(string UserId)
+        {
+            if (string.IsNullOrWhiteSpace(UserId))
+                return null;
+            try
+            {
+                return _reviewComments.Table.Where(c => c.IsApproved == true && c.IsActive == true).ToList();
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+        }
+        #endregion
+    }
+}
